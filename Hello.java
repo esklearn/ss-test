46a4c9e251db81ba1cec6af738193413143a9b06
esk <santhu.esk@gmail.com>
2018-08-14 23:47:52 +0100

    Squashed commit of the following:
    commit 87dc0204bb786958d99cf35538d9be0dbc4443ac
    Author: esk <santhu.esk@gmail.com>
    Date:   Tue Aug 14 23:40:06 2018 +0100
    
        Added method

--- Hello.java
+++ Hello.java
@@ -3,5 +3,12 @@ class Hello {
 		System.out.println("Hello " + args[0] + "!!");
 		System.out.println("Hello " + args[0] + "!! - Second time");
 		System.out.println("Hello " + args[0] + "!! - Third time");
+		System.out.println(greeting(args[0]));
 	}
+
+
+	public static String greeting(String name){
+			return "Welcome " + name;
+		
+		}
 }
